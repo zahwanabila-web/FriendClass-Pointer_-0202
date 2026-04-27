d3d82735bb861b4edbcac779af3163f72e48193e
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 20:59:50 +0700

    commit15

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -40,3 +40,25 @@ public:
         return 4 * sisi;
     }
 };
+
+double hitungKeliling(LayangLayang ll) {
+    double sisi1 = (ll.diagonal1/2, 2) + (ll.diagonal2/2, 2);
+    return 2 * (sisi1 + sisi1);
+}
+int main() {
+    LayangLayang layang;
+    BelahKetupat ketupat;
+
+    layang.input();
+    cout << fixed << (2);
+    cout << "HASIL LAYANG-LAYANG:" << endl;
+    cout << "Luas     = " << layang.luas() << endl;
+    cout << "Keliling = " << hitungKeliling(layang) << endl;
+    
+    ketupat.input();
+    cout << "HASIL BELAH KETUPAT:" << endl;
+    cout << "Luas     = " << ketupat.luas() << endl;
+    cout << "Keliling = " << ketupat.keliling() << endl;
+    
+    return 0;
+}
